c993df99f531466fde11f34c929da4c68033408d
tilovashahrin <tilova.shahrin@ontariotechu.net>
2021-05-09 15:50:08 -0400

    style

--- scripts/forms.js
+++ scripts/forms.js
@@ -10,15 +10,16 @@ $(document).ready(()=> {
         e.preventDefault();
     });
 
-    $("#donebtn").on('click', (e) =>{
-        e.preventDefault();
-    });
 })
 
     function apiCall(searchtxt){
         var $selected = $('#movie_details');
         var $kids = $selected.children();
         $kids.remove();
+
+        // var $selected2 = $('#results-title');
+        // var $kids2 = $selected2.children();
+        // $kids2.remove();
         axios.get('http://www.omdbapi.com/?apikey=7aefcf4e&s='+searchtxt).then((response) => {
                 let mov_list = response.data.Search;
                 let output = '';
@@ -28,6 +29,7 @@ $(document).ready(()=> {
                     <button type="button" id="nominatebtn" onclick="nominate('${movies.Title}', '${movies.Year}')">Nominate</button>
                     `;
                 })
+                //$('#results-title').append('Results for "'+ searchtxt +'"');
                 $('#movie_details').append(output);
 
             })
@@ -39,17 +41,15 @@ $(document).ready(()=> {
     function nominate(title, year){       
         var $selected = $('#nominations');
         var $kids = $selected.children();
-        if($kids.length >= 1){      
-            document.getElementById("donebtn").style.display = "block";
-        }
         if ($kids.length >= 5){         
             alert("Can only have 5 nominations");
         }else{
             let output = `
                 <div id="nom_result">
-                <h5 id="movie_nom"> `+title+ ` (`+year+`)</h5> 
+                <h5 id="movie_nom"> `+ title + ` (`+ year +`)</h5> 
                 <button type="button" id="removebtn" onclick="remove()">Remove</button>
-                </div>`;
+                </div>
+                `;
             $('#nominations').append(output);
         }
         
@@ -58,10 +58,4 @@ $(document).ready(()=> {
     function remove(){
         var div = document.getElementById('nom_result');
         div.parentNode.removeChild(div);
-    }
-
-    function done(){
-        let $nom_list = $('#movie_nom');
-        //var $kids = $nom_list.children();
-        console.log($nom_list);
     }
